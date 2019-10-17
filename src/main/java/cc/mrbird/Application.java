91e7ccff292f02526011162de2bec9e0605a23be
tspeking <tspeking@163.com>
2019-10-17 11:13:00 +0800

    test commit

--- src/main/java/cc/mrbird/Application.java
+++ src/main/java/cc/mrbird/Application.java
@@ -20,5 +20,6 @@ public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
                 .run(args);
+        //test commit
     }
 }
